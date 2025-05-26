2a33b5e9f826f66811663a64930044c64197f3ad
Mohamed Ragab <m.ragab6010@gmail.com>
2025-05-26 23:40:32 +0200

    fix: Remove unused code from plugin.go and document deps command - Remove unused pluginOperation type and executePluginOperation function - Remove unused imports - Add documentation for plugin deps command in README.md - Fix golangci-lint unused code warnings

--- cmd/cluster/plugin/plugin.go
+++ cmd/cluster/plugin/plugin.go
@@ -1,9 +1,6 @@
 package plugin
 
 import (
-	"github.com/mrgb7/playground/internal/plugins"
-	"github.com/mrgb7/playground/pkg/logger"
-	"github.com/mrgb7/playground/types"
 	"github.com/spf13/cobra"
 )
 
@@ -11,46 +8,6 @@ var PluginCmd = &cobra.Command{
 	Use:   "plugin",
 	Short: "Manage plugins",
 	Long:  `Manage plugins for the cluster`,
-}
-
-type pluginOperation func(plugin plugins.Plugin, kubeConfig, clusterName string) error
-
-func executePluginOperation(pluginName, clusterName string, operation pluginOperation,
-	successMsg, errorMsg string) error {
-	c := types.Cluster{
-		Name: clusterName,
-	}
-
-	ip := c.GetMasterIP()
-	if err := c.SetKubeConfig(); err != nil {
-		logger.Errorln("Failed to set kubeconfig: %v", err)
-		return err
-	}
-
-	pluginsList, err := plugins.CreatePluginsList(c.KubeConfig, ip, c.Name)
-	if err != nil {
-		logger.Errorln("Failed to create plugins list: %v", err)
-		return err
-	}
-
-	for _, plugin := range pluginsList {
-		if plugin.GetName() == pluginName {
-			err := operation(plugin, c.KubeConfig, c.Name)
-			if err != nil {
-				logger.Errorln("%s: %v", errorMsg, err)
-				return err
-			}
-			logger.Successln(successMsg, pluginName)
-			return nil
-		}
-	}
-
-	logger.Errorln("Plugin %s not found", pluginName)
-	logger.Infoln("Available plugins:")
-	for _, plugin := range pluginsList {
-		logger.Infoln("  - %s", plugin.GetName())
-	}
-	return nil
 }
 
 func init() {
